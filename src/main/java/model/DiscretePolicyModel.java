21c380b23213f232e8036ae07b9b06e09e38d245
caojunqi <caojunqi@foxmail.com>
2021-09-23 10:14:55 +0800

    精简代码，删除冗余的模型设计。

--- src/main/java/model/DiscretePolicyModel.java
+++ src/main/java/model/DiscretePolicyModel.java
@@ -1,4 +1,4 @@
-package model.two;
+package model;
 
 import ai.djl.Model;
 import ai.djl.ndarray.NDArray;
@@ -16,39 +16,40 @@ import ai.djl.training.initializer.Initializer;
 import ai.djl.training.initializer.UniformInitializer;
 import ai.djl.training.initializer.XavierInitializer;
 import ai.djl.util.PairList;
-import model.BaseModel;
 
 /**
- * 状态价值函数近似模型
+ * 用于生成离散型动作的策略模型
  *
  * @author Caojunqi
- * @date 2021-09-16 18:06
+ * @date 2021-09-16 15:40
  */
-public class CriticValueModel extends BaseModel {
+public class DiscretePolicyModel extends BaseModel {
+    private int actionNum;
     private int[] hiddenSize;
     private Block affineLayer;
-    private Block valueHead;
+    private Block actionHead;
 
-    private CriticValueModel(NDManager manager, int[] hiddenSize) {
+    private DiscretePolicyModel(NDManager manager, int actionNum, int[] hiddenSize) {
         super(manager);
         SequentialBlock affineLayers = new SequentialBlock();
         for (int hiddenNum : hiddenSize) {
             affineLayers.add(Linear.builder().setUnits(hiddenNum).build());
             affineLayers.add(Activation::tanh);
         }
+        this.actionNum = actionNum;
         this.hiddenSize = hiddenSize;
         this.affineLayer = addChildBlock("affine_layer", affineLayers);
-        this.valueHead = addChildBlock("value_head", Linear.builder().setUnits(1).build());
+        this.actionHead = addChildBlock("action_head", Linear.builder().setUnits(actionNum).build());
     }
 
-    public static Model newModel(NDManager manager, int stateDim) {
+    public static Model newModel(NDManager manager, int stateDim, int actionNum) {
         int[] hiddenSize = new int[]{128, 128};
-        return newModel(manager, stateDim, hiddenSize);
+        return newModel(manager, stateDim, actionNum, hiddenSize);
     }
 
-    public static Model newModel(NDManager manager, int stateDim, int[] hiddenSize) {
-        Model model = Model.newInstance("critic_value_model");
-        BaseModel net = new CriticValueModel(manager, hiddenSize);
+    public static Model newModel(NDManager manager, int stateDim, int actionNum, int[] hiddenSize) {
+        Model model = Model.newInstance("discrete_policy_model");
+        BaseModel net = new DiscretePolicyModel(manager, actionNum, hiddenSize);
         net.initialize(net.getManager(), DataType.FLOAT32, new Shape(stateDim));
         model.setBlock(net);
         return model;
@@ -57,13 +58,14 @@ public class CriticValueModel extends BaseModel {
     @Override
     protected NDList forwardInternal(ParameterStore parameterStore, NDList inputs, boolean training, PairList<String, Object> params) {
         NDList hidden = new NDList(affineLayer.forward(parameterStore, inputs, training).singletonOrThrow());
-        NDArray value = valueHead.forward(parameterStore, hidden, training).singletonOrThrow();
-        return new NDList(value);
+        NDArray scores = actionHead.forward(parameterStore, hidden, training).singletonOrThrow();
+        NDArray distribution = scores.softmax(scores.getShape().dimension() - 1);
+        return new NDList(distribution);
     }
 
     @Override
     public Shape[] getOutputShapes(Shape[] inputShapes) {
-        return new Shape[]{new Shape(1)};
+        return new Shape[]{new Shape(actionNum)};
     }
 
     @Override
@@ -71,8 +73,9 @@ public class CriticValueModel extends BaseModel {
         this.affineLayer.setInitializer(new XavierInitializer(), Parameter.Type.WEIGHT);
         this.affineLayer.initialize(manager, dataType, inputShapes[0]);
 
-        this.valueHead.setInitializer(new UniformInitializer(), Parameter.Type.WEIGHT);
-        this.valueHead.setInitializer(Initializer.ZEROS, Parameter.Type.BIAS);
-        this.valueHead.initialize(manager, dataType, new Shape(hiddenSize[hiddenSize.length - 1]));
+        this.actionHead.setInitializer(new UniformInitializer(), Parameter.Type.WEIGHT);
+        this.actionHead.setInitializer(Initializer.ZEROS, Parameter.Type.BIAS);
+        this.actionHead.initialize(manager, dataType, new Shape(hiddenSize[hiddenSize.length - 1]));
     }
+
 }
