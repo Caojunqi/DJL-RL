e355bffe94b4da2ab01ab633ca18f73eb39e73a0
caojunqi <caojunqi@foxmail.com>
2021-09-30 11:30:07 +0800

    优化收益输出格式，便于查看模型是否收敛。

--- src/main/java/utils/Runner.java
+++ src/main/java/utils/Runner.java
@@ -37,6 +37,9 @@ public class Runner<A extends Action, E extends Environment<A>> {
         agent.resetMemory();
         int sampleNum = 0;
         int episodesNum = 0;
+        float totalReward = 0;
+        float minEpisodeReward = Float.MAX_VALUE;
+        float maxEpisodeReward = Float.MIN_VALUE;
         while (sampleNum < minBatchSize) {
             float[] state = env.reset().clone();
             boolean done = false;
@@ -47,18 +50,23 @@ public class Runner<A extends Action, E extends Environment<A>> {
                 env.render();
                 A action = agent.selectAction(state);
                 Snapshot snapshot = env.step(action);
-                episodeReward += snapshot.getReward();
                 agent.collect(state, action, snapshot.isDone(), snapshot.getNextState(), snapshot.getReward());
 
                 done = snapshot.isDone();
                 state = snapshot.getNextState().clone();
                 step++;
+
+                episodeReward += snapshot.getReward();
             }
+
+            totalReward += episodeReward;
+            minEpisodeReward = Math.min(minEpisodeReward, episodeReward);
+            maxEpisodeReward = Math.min(maxEpisodeReward, episodeReward);
 
             sampleNum += step;
             episodesNum += 1;
-
-            System.out.println("episode[" + episodesNum + "], reward[" + episodeReward + "]");
         }
+
+        System.out.println("AverageEpisodeReward [" + (totalReward / episodesNum) + "] MaxEpisodeReward [" + maxEpisodeReward + "] MinEpisodeReward [" + minEpisodeReward + "]");
     }
 }
