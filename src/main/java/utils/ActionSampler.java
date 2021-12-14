b70e88970a08c4c9d98c536bfcae7856dc213635
caojunqi <caojunqi@foxmail.com>
2021-12-14 21:31:52 +0800

    perf(框架): 优化行为随机选择逻辑，一旦选择出结果后就应该立刻停止搜索，提高效率。

--- src/main/java/utils/ActionSampler.java
+++ src/main/java/utils/ActionSampler.java
@@ -74,6 +74,7 @@ public final class ActionSampler {
                     value++;
                 } else {
                     actionData[i] = value;
+                    break;
                 }
                 rnd -= cut;
             }
